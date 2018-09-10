92e862f5914007e959004a93a5d55c49295ef7f7
Abdulrhman <abooooody40@gmail.com>
2018-09-10 18:16:41 +0300

    Add Client.js

---
+++ src/Client.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+class Client {
+
+  constructor(client) {
+    if(!client instanceof Discord.Client) throw new Error('The argument must be a discord client')
+    this.client = client;
+  }
+
+  constructor(){
+    throw new Error('You must enter a discord client in argument.')
+  }
+
+}
